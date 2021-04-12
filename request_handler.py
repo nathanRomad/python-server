832340d3e42b3aa751d461e1531ddbab6264a95d
nathanRomad <nathan.a.hamilton@gmail.com>
2021-04-12 09:46:34 -0500

    feat: delete functionality added for locattions

--- request_handler.py
+++ request_handler.py
@@ -1,10 +1,9 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer
 from employees import get_all_employees, get_single_employee, create_employee, delete_employee
-from locations import get_all_locations, get_single_location, create_location
-import json
-
+from locations import get_all_locations, get_single_location, create_location, delete_location
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
