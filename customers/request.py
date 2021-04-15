ad2a69b49840426188fe164473e4e956b4decb39
nathanRomad <nathan.a.hamilton@gmail.com>
2021-04-15 13:23:51 -0500

    feat: handling query string params

--- customers/request.py
+++ customers/request.py
@@ -2,55 +2,6 @@ import sqlite3
 import json
 from models import Customer
 
-# CUSTOMERS = [
-#     {
-#       "id": 1,
-#       "name": "Nathan Hamilton",
-#       "address": "0101 Hacker Ct",
-#       "email": "nate@nss.com",
-#       "password": ""
-#     },
-#     {
-#       "id": 2,
-#       "name": "Alexa Klein",
-#       "address": "1 Babes Way",
-#       "email": "babe@home.com",
-#       "password": ""
-#     },
-#     {
-#       "id": 3,
-#       "name": "Elliot Alderson",
-#       "address": "001 Mr. Robot Drive",
-#       "email": "",
-#       "password": ""
-#     },
-#     {
-#       "id": 4,
-#       "name": "Neo",
-#       "address": "1 Wall Street Court",
-#       "email": "",
-#       "password": ""
-#     }
-#   ]
-
-  # Function with a single parameter
-# def get_single_customer(id):
-#     # Variable to hold the found customer, if it exists
-#     requested_customer = None
-
-#     # Iterate the customerS list above. Very similar to the
-#     # for..of loops you used in JavaScript.
-#     for customer in CUSTOMERS:
-#         # Dictionaries in Python use [] notation to find a key
-#         # instead of the dot notation that JavaScript used.
-#         if customer["id"] == id:
-#             requested_customer = customer
-
-#     return requested_customer
-
-# def get_all_customers():
-#     return CUSTOMERS
-
 def get_single_customer(id):
     with sqlite3.connect("./kennel.db") as conn:
         conn.row_factory = sqlite3.Row
@@ -81,6 +32,7 @@ def get_single_customer(id):
 # query the database for all customers, 
 # convert each row into an customer instance, 
 # convert the list to JSON, and respond to the client request.
+
 def get_all_customers():
     # Open a connection to the database
     with sqlite3.connect("./kennel.db") as conn:
@@ -160,3 +112,29 @@ def update_customer(id, new_customer):
             # Found the customer. Update the value.
             CUSTOMERS[index] = new_customer
             break
+
+def get_customers_by_email(email):
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        select
+            c.id,
+            c.name,
+            c.address,
+            c.email,
+            c.password
+        from Customer c
+        WHERE c.email = ?
+        """, ( email, ))
+
+        customers = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            customer = Customer(row['id'], row['name'], row['address'], row['email'] , row['password'])
+            customers.append(customer.__dict__)
+
+    return json.dumps(customers)
